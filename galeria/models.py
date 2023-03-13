a2b7a8609fce394f4bed49dfa47b315a765c55b7
unknown <you@example.com>
2023-03-13 20:09:56 -0300

    aprendendo sobre ORM

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#Classes que serão convertidas nas tabelas do banco
+
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+
+#retorna o nome do objeto
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}"
--- galeria/views.py
+++ galeria/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 
 ## DON PRETON
 #criando metodo para responder a requisição e abrir o link solicitado
+
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    # DADOS DAS IMAGENS
+    dados = {
+        1:{"nome": "Nebulosa de Carina",
+         "legenda": "webtelescope.org / NASA/ James Webb"},
+        2:{"nome": "Galaxia NGC 1079", 
+        "legenda" : "nasa.org / NASA / Hubble"}
+    }
+    return render(request, 'galeria/index.html', {"cards" : dados} )
 
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
+
